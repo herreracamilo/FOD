f71a8e847787e25b221c23111f4d307589e9c42c
Camilo <7camiloherrera@gmail.com>
2024-04-23 13:31:25 -0300

    EJE2 practica3

--- Practica3/EJE2/EJE2.pas
+++ Practica3/EJE2/EJE2.pas
@@ -8,3 +8,116 @@ Para ello se podrá utilizar algún carácter especial situándolo delante de al
 String a su elección. Ejemplo: ‘@Saldaño’.*)
 
 program EJE2;
+type
+    asistente = record
+        num:integer;
+        apellido:string[20];
+        nombre:string[20];
+        email:string[70];
+        telefono:integer;
+        dni:integer;
+    end;
+    
+    maestro = file of asistente;
+
+    procedure leerAsis(var a:asistente);
+    begin
+        with a do begin
+            writeln('ingrese numero de asistente');
+            readln(num);
+            writeln('ingrese apellido');
+            readln(apellido);
+            writeln('ingrese nombre');
+            readln(nombre);
+            writeln('ingrese email');
+            readln(email);
+            writeln('ingrese tel');
+            readln(telefono);
+            writeln('ingrese dni');
+            readln(dni);
+        end;
+    end;
+
+    procedure crearMae(var mae:maestro);
+    var
+        a:asistente;
+        texto:text;
+    begin
+        assign(mae,'maestro');
+        assign(texto,'asistente.txt');
+        rewrite(mae);
+        reset(texto);
+
+        while not eof (texto)do begin
+            with a do begin
+                readln(texto,apellido);
+                readln(texto,nombre);
+                readln(texto,num,telefono,dni,email);
+                write(mae,a);
+            end;
+        end;
+        close(mae);
+        close(texto);
+        writeln('=== maestro creado ===');
+    end;
+
+    procedure exportar (var mae:maestro);
+    var
+        texto:text;
+        a:asistente;
+        pregunta:string;
+    begin
+        assign(mae,'maestro');
+        reset(mae);
+
+        writeln('como se va a llamar el archivo exportado');
+        readln(pregunta);
+        assign(texto, pregunta);
+        rewrite(texto);
+
+        while not eof (mae)do begin
+            with a do begin
+                read(mae,a);
+                writeln(texto, 'apellido: ', apellido);
+                writeln(texto, 'nombre: ', nombre);
+                writeln(texto, 'num empleado: ', num);
+                writeln(texto, 'email: ', email);
+                writeln(texto, 'telefono: ', telefono);
+                writeln(texto, 'dni: ', dni);
+                writeln(texto);
+            end;
+        end;
+        close(mae);
+        close(texto);
+        writeln('=== el archivo se exporto correctamente ===');
+    end;
+
+    procedure borradoLogico(var mae:maestro);
+    var
+        a:asistente;
+        aux:string;
+    begin
+        assign(mae,'maestro');
+        reset(mae);
+
+        while not eof(mae)do begin
+            read(mae,a);
+            if(a.num < 1000)then begin
+                aux:='@'+a.nombre;
+                a.nombre:=aux;
+                seek(mae,filepos(mae)-1);
+                write(mae,a);
+            end;
+        end;
+        close(mae);
+        writeln('=== borrados logicos con exito ===');
+    end;
+
+var
+    mae:maestro;
+begin
+    crearMae(mae);
+    exportar(mae);
+    borradoLogico(mae);
+    exportar(mae);
+end.
